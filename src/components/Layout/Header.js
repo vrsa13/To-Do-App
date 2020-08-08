b116021fbe4a2c093f9b8f8945cca1701a3f8b10
Varsha <maranvrsa@gmail.com>
2020-08-08 17:42:28 +0530

    First commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+        <header style={headerStyle}>
+           <h1>TodoList</h1> 
+           <Link style={LinkStyle} to="/">Home</Link> | <Link 
+           style={LinkStyle} to="/about">About</Link>
+        </header>
+    )
+}
+
+const headerStyle ={
+    background: '#333',
+    color: '#fff',
+    textAlign:'center',
+    padding: '10px'
+}
+
+const LinkStyle = {
+    color: '#fff',
+    textDecoration:'none'
+}
+
+export default Header;
